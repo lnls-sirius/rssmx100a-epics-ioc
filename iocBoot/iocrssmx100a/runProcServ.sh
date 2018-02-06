4d93dbc22f41a3805fba4a24e9709e96c30b2aac
Leticia Garcez Capovilla <leticiagc@unifei.edu.br>
2018-02-06 09:20:38 -0200

    [iocBoot] runProcServ.sh: use specified TELNET port

--- iocBoot/iocrssmx100a/runProcServ.sh
+++ iocBoot/iocrssmx100a/runProcServ.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+# Use defaults is not set
+if [ -z "${RSSMX100A_DEVICE_TELNET_PORT}" ]; then
+    RSSMX100A_DEVICE_TELNET_PORT=20000
+fi
+
 # Run run*.sh scripts with procServ
-/usr/local/bin/procServ -f -n RSSMX100A -i ^C^D 20000 ./runGenericSMX.sh "$@"
+/usr/local/bin/procServ -f -n rssmx100a${RSSMX100A_INSTANCE} -i ^C^D ${RSSMX100A_DEVICE_TELNET_PORT} ./runRSSMX100A.sh "$@"
